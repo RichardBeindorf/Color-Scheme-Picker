4475ed2a9f7525127686ba5af07b83fb39088d82
Richard Beindorf <richard_beindorf@web.de>
2024-04-03 11:36:06 +0200

    fixed file structure and impots of components

--- src/components/SchemeList/SchemeList.js
+++ src/components/SchemeList/SchemeList.js
@@ -1,6 +1,9 @@
 import "./SchemeList.css";
-import ColorList from "../ColorList"
+import ColorList from "../ColorList/ColorList"
 
 export default function SchemeList(){
-
+    return <>
+    <h2>Scheme Title</h2>
+    < ColorList />
+    </>
 }

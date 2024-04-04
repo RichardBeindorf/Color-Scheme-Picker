a2c621268c46549c9241544def0ba80cfcedc730
Richard Beindorf <richard_beindorf@web.de>
2024-04-04 10:02:37 +0200

    minor design changes on cards

--- src/components/Card/ColorRole/ColorRole.js
+++ src/components/Card/ColorRole/ColorRole.js
@@ -2,7 +2,7 @@ import "./ColorRole.css";
 
 export default function ColorRole({ role, value }){
     return <div className="color-role">
-        <h5>{ role }</h5>
-        <p>{ value }</p>
+        <h5 className="color-role">{ role }</h5>
+        <p className="hex-value">{ value }</p>
     </div>
 }

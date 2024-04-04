81969694171d77b4a6afca4e1f3ed1a1a3435885
Richard Beindorf <richard_beindorf@web.de>
2024-04-04 13:29:06 +0200

    working with state now

--- src/components/SchemeList/SchemeList.js
+++ src/components/SchemeList/SchemeList.js
@@ -2,17 +2,23 @@ import "./SchemeList.css";
 import Cards from "../Card/Cards";
 import { initialThemes } from "../../db";
 import { useState } from "react";
-import { SingleColors } from "../Previews/Preview"
+import { SingleColors } from "../Previews/Preview";
 
 export default function SchemeList() {
   const [themes, setThemes] = useState(initialThemes);
+  const [showDetails, setShowDetails] = useState(false);
 
   return themes.map((theme) => (
     <>
       <ul className="scheme-list" key={theme.id}>
         <h2>{theme.name}</h2>
-        <ul className="preview-colors"><SingleColors colors={theme.colors}/></ul>
-        <Cards theme={theme.colors} />
+        <ul className="preview-colors">
+          {" "}
+          <SingleColors
+            colors={theme.colors}
+          />{" "}
+        </ul>
+        <Cards theme={theme.colors} showDetails={showDetails}/>
       </ul>
     </>
   ));
